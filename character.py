287e000d8e6168d45f10aaab19e4578b080eb693
Nils Nordström <nnx@gmx.com>
2024-10-21 09:11:24 +0200

    ny metod

--- character.py
+++ character.py
@@ -1,11 +1,9 @@
-roles = ["Warrior", "Mage", "Rogue", "Paladin", "Ranger"]
-
 class weapon: 
     def __init__(self, weapon, base_dmg, difficulty): 
         print(weapon, base_dmg, difficulty) 
   
 class character:
-    def __init__(self, name, hp, weapon_name):
+    def __init__(self, name, hp, weaponName, weaponDamage):
         self.name = name
         self.hp = hp
-        self.obj1 = weapon(weapon_name, 20, 50)
+        self.obj1 = weapon(weaponName, weaponDamage, 100-weaponDamage)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from character import character as char, weapon
+from character import character as char
 import random as rand
 
 adjective = ["The Great", "The Evil", "The Furious",
@@ -13,35 +13,34 @@ weapons = ["sword", "axe", "bow",
            "dagger", "spear", "crossbow",
            "mace", "hammer"]
 
+####metoder
+
+def ReadInt(message, min, max):
+    while True:
+        userInput = input(message).lower()
+        try:
+            userInput = int(userInput)
+            if userInput < min or userInput > max:
+                raise
+            return userInput
+        except:
+            print(f"Invalid input! Must be a whole number in the range {min}-{max}")
+            continue
+
+##########
+
 for item in weapons:
     print(f'{weapons.index(item)+1} {item}')
+    
+playerWeapon = weapons[(ReadInt("Choose your weapon: ", 0, 8))-1]
 
-while True:
-    weapon_choice = input("Choose your weapon: ").lower().strip()
-    try:
-        index = int(weapon_choice)
-        print(index)
-        player_weapon = weapons[index-1]
-        break
-    except ValueError:
-        if weapon_choice not in weapons:
-            print("Invalid answer")
-            continue
-        player_weapon = weapons[weapons.index(weapon_choice)]
-        break
-    except IndexError:
-        print("Invalid answer")
-        continue
-
-weapon_damage = input("tune your weapon with 100 points, more damage = less accuracy: ")
-
-plr = char("nils", 100, player_weapon)
+weaponDamage = ReadInt("tune your weapon with 100 points, more damage = less accuracy: ", 0, 100)
+plr = char("nils", 100, playerWeapon, weaponDamage)
 
 #foe saker
 #foe_name = f'{rand.choice(adjective)} {rand.choice(races)}'
 #print(foe_name)
 #foe = char(foe_name, 100, weapon)
-
 #klasser:
 #klass
 #attacker
