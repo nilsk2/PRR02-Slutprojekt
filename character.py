72062eb3fa2b89a1e200dde4ebc4aedcabb6689f
Nils Nordström <nnx@gmx.com>
2025-01-12 23:47:34 +0100

    lagt till mer komplex algoritm "defense" och avrundar alla tal som printas

--- character.py
+++ character.py
@@ -14,28 +14,31 @@ class Weapon:
         return self.__difficulty
   
 class Character:
-    def __init__(self, name, hp, weaponDamage):
+    def __init__(self, name, hp, defense, weaponDamage):
         self.__name = name
         self.__hp = hp
+        self.__defense = defense
         self.__charWeapon = Weapon(weaponDamage, 100-weaponDamage)
 
-    def attack(self, target): #fixa 
+    def attack(self, target):
         damage = self.charWeapon.baseDmg
-        randomValue = rand.randrange(0,100)
+        randomValue = rand.randrange(0, 100)
         if randomValue < 10: 
-            target.takeDamage(damage*2)
             print("Critical hit!")
-            print(f"{damage*2} damage is delt by {self.name}")
+            print(f"{round(damage * 2, 2)} damage is dealt by {self.name}")
+            target.takeDamage(damage * 2)
         elif randomValue < self.charWeapon.difficulty:
+            print(f"{round(damage, 2)} damage is dealt by {self.name}")
             target.takeDamage(damage)
-            print(f"{damage} damage is delt by {self.name}")
         else:
-            target.takeDamage(0)
             print(f"{self.name}'s attack misses")
+            target.takeDamage(0)
 
     def takeDamage(self, damage):
-        self.hp -= damage
-        if self.hp < 0: self.hp = 0
+        reducedDamage = max(0, round(damage * self.__defense, 2))
+        self.hp -= reducedDamage
+        print(f'but {round(damage - reducedDamage, 2)} damage is blocked!')
+        print(f'Total damage taken: {reducedDamage}')
 
     @property
     def name(self):
@@ -44,7 +47,18 @@ class Character:
     @property
     def hp(self):
         return self.__hp
+
+    @hp.setter
+    def hp(self, value):
+        if value < 0:
+            self.__hp = 0
+        else:
+            self.__hp = value
     
+    @property
+    def defense(self):
+        return self.__defense
+
     @property
     def charWeapon(self):
         return self.__charWeapon
